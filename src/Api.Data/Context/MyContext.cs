e2f730f8e2b59242cb8aaceb88f9d3eb31c42d96
Daniel V <daniel.v.veronez@gmail.com>
2020-10-06 19:53:52 -0300

    Adicionando usuário ADM automaticamente

--- src/Api.Data/Context/MyContext.cs
+++ src/Api.Data/Context/MyContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Api.Data.Mapping;
 using Api.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,17 @@ namespace Api.Data.Context
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity{
+                    Id = Guid.NewGuid(),
+                    Name = "Administrador",
+                    Email = "testeadm@teste.com",
+                    CreatAt = DateTime.MinValue,
+                    UpdateAt = DateTime.MinValue,
+                    Password = ""
+                }
+            );
         }
     }
 }
